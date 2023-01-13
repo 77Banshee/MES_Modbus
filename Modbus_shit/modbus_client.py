278aa553ea60301d6d6d437510a4b95baf24f9ba
bns_work <tweek693@gmail.com>
2023-01-14 01:43:34 +0700

    read inc, tk

--- Modbus_shit/modbus_client.py
+++ Modbus_shit/modbus_client.py
@@ -2,6 +2,18 @@ from pyModbusTCP.client import ModbusClient
 import struct
 import json
 
+#192.168.9.
+    #..
+    #..
+    #13
+    #14
+    #15
+    #16
+    #..
+    #..
+    #19
+    #20
+
 class Converter_Repository():
     def __init__(self, path_to_cfg: str):
         self.converter_list = []
@@ -10,17 +22,17 @@ class Converter_Repository():
         self.init_converter_list()
 
     def init_converter_list(self):
-        converter_list_raw = self.json_config['converter']
-        for i in converter_list_raw:
+        converter_list_raw = self.json_config["converters"]
+        for i in range(0, len(converter_list_raw)):
             self.converter_list.append(
-                Zet_Converter(
-                    i['ip_address'],
-                    i['name'],
-                    i['devices']
+                Zet7076_Converter(
+                    ip_address=converter_list_raw[i]['ip_address'],
+                    name=converter_list_raw[i]['name'],
+                    sensor_list_raw=converter_list_raw[i]['devices']
                 )
             )
 
-class Zet_Converter(object):
+class Zet7076_Converter(object):
     def __init__(self, ip_address: str, name: str, sensor_list_raw, port: int=502):
         self.ip_address = ip_address
         self.name = name
@@ -33,50 +45,85 @@ class Zet_Converter(object):
 
     def init_devices(self, sensor_list_raw):
         for i in sensor_list_raw:
-            match i['type']:
-                case 'ZET_7054_Inclinometer':
+            if i['type'] == 'ZET_7054_Inclinometer':
                     self.devices.append(
                         Zet_Inclinometer(
-                            i['id'], i['slave'], i['registers']['x'], i['registers']['y']
+                            id=i["id"],
+                            type=i["type"],
+                            name=i["name"],
+                            slave=i['slave'],
+                            x_registers=i['registers']['x'], 
+                            y_registers=i['registers']['y']
                         )
                     )
-                case 'Thermometer':
+            elif i['type'] == 'Thermometer':
                     raise NotImplementedError('Zet_Converter.init_devices() THERMOMTER NOT IMPLEMENT')
 
-    def receive(self):
+    def receive_all(self):
         if not self.devices:
             print(f"{self.id} device list is empty!")
             return
-        client = ModbusClient(host=self.ip_address, port=self.port, unit_id=3)  # 0x03 register.
-        x_high = client.read_holding_registers(self.y_registers[1])  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
-        x_low = client.read_holding_registers(self.y_registers[0])  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
+        print(f"[*] Process converter: {self.name}")
+        for i in self.devices:
+            if i.type == "ZET_7054_Inclinometer":
+                    print(f"[*] Process device: {i.name} slave: {i.slave_number}")
+                    client = ModbusClient(host=self.ip_address, port=self.port, unit_id=i.slave_number) #Slave?
+                    x_high = client.read_holding_registers(i.x_registers[1])  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
+                    x_low = client.read_holding_registers(i.x_registers[0])  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
+                    y_high = client.read_holding_registers(i.y_registers[1])  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
+                    y_low = client.read_holding_registers(i.y_registers[0])  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
 
+                    if None in [x_high, x_low, y_high, y_low]:
+                        print("Null received. Abort...")
+                        continue
+                    
+                    x_decoded = i.decode(x_high[0], x_low[0])
+                    y_decoded = i.decode(y_high[0], y_low[0])
+                    print(f"x_raw: {x_decoded}")
+                    print(f"y_raw: {y_decoded}")
+                    
+                    # print(f"{i.name} {i.type} | x: {i.decode(x_raw)} | y: {i.decode(y_raw)}")
+            else:
+                raise ValueError("[*] Wrong device type!")
+# x_raw: [[16408], [43274]]
+# y_raw: [[49431], [11310]]       
 
 class Zet_Inclinometer(object):
-    def __init__(self, id: str, slave: int, x_registers: [], y_registers: []):
-        self.id = id,
+    def __init__(self, id: str, slave: int, name, type, x_registers, y_registers):
+        [self.id] = id,
         self.x_registers = x_registers
         self.y_registers = y_registers
         self.slave_number = slave
+        self.name = name
+        self.type = type
 
     def __str__(self):
         return f"Zet_Inclinomter: ID: {self.id}, x_registers: {self.x_registers}, y_registers: {self.y_registers}, slave_number: {self.slave_number}"
 
-    def to_float(self, raw_values):
-        to_bytes = struct.pack('>HH', raw_values[1], raw_values[0])
+    def decode(self, high, low):
+        to_bytes = struct.pack('>HH', high, low)
         float_res = struct.unpack('>f', to_bytes)
         return float_res[0]
 
-    # def receive(self):
-    #     client = ModbusClient(host="192.168.9.11", port=502, unit_id=3)  # 0x03 register.
+    # Do i even need it?
+    # def receive(self, ip, port, unit_id):
+    #     client = ModbusClient(ip, port, unit_id)  # 0x03 register.
     #     x_raw_values = client.read_holding_registers(self.x_registers[0], 2)  # В struct pack/unpack передавать сначала 21 потом 20 регистр.
     #     y_raw_values = client.read_holding_registers(self.y_registers[0], 2)
     #     return self.to_float(x_raw_values), self.to_float(y_raw_values) # X, Y
 
+
 if __name__ == '__main__':
     print("__Modbus_client__")
+    print("[* Init...]")
     cr = Converter_Repository("Config/converter.json")
+    print("[* Config has loaded!]")
     for i in cr.converter_list:
-        print(i)
+        
+        print(f"Converter: {i}")
         for j in i.devices:
-            print(j)
+            print(f"\tDevice: {j}")
+
+    print("Start receive...")
+    for i in cr.converter_list:
+        i.receive_all()
